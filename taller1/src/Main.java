cb4cd686f65f0e6c783e57e2ce7272daad698df1
Javier <j.alcalde01@ufromail.cl>
2023-08-24 09:31:52 -0400

    Version 1 9:30

---
+++ taller1/src/Main.java
@@ -0,0 +1,23 @@
+package taller1.src;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+
+    }
+
+    public static int scanner(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese un numero del 0 al 9");
+        int num = scanner.nextInt();
+        return num;
+    }
+
+    public static boolean validarNumeros(int scanner(), int scanner()){
+
+    }
+
+    public static int matriz(){
+
+    }
+
+}
